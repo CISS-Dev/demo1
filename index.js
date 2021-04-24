32b955ee034cd872bf111ce884cee7f64e8592b1
Michael Reschke <michael.reschke.koeln@gmail.com>
2021-04-24 18:12:01 +0200

    second commit

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const path = require('path')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-});
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-const port = process.env.PORT || 4000
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 
+const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
